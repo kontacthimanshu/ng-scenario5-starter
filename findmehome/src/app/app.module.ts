fb47a1faddce6848a155081852d911272edb2c9e
kontacthimanshu@hotmail.com <kontacthimanshu@hotmail.com>
2020-07-31 11:42:19 +0530

    applied scenario 4 starter steps

--- findmehome/src/app/app.module.ts
+++ findmehome/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './properties/properties-list.component';
+import { PropertyThumbnailComponent } from './properties/property-thumbnail.component';
+import { NavBarComponent } from './nav/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyListComponent,
+    PropertyThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
